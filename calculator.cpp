8251549bdb16ca3333b63486612a01b6cd4fb458
protein2005 <alexeysokole@gmail.com>
2024-09-23 19:03:49 +0300

    pow operation

--- calculator.cpp
+++ calculator.cpp
@@ -14,3 +14,13 @@ int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+
+int Calculator::Pow(double a, int exp)
+{
+    double result = 1;
+    for (int i = 0; i < exp; ++i)
+    {
+        result *= a;
+    }
+    return result + 0.5;
+}
--- calculator.h
+++ calculator.h
@@ -7,6 +7,7 @@ class Calculator
         int Add (double, double);
         int Sub (double, double);
         int Mul (double, double);
+        int Pow(double, int);
 };
 
 #endif//CALCULATOR_H
